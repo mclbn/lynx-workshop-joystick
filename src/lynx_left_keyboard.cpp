9e1c8c64204257b3dc48979ecab85a84a4f5d606
Marc Lebrun <mclbn@users.noreply.github.com>
2023-11-08 20:58:47 +0100

    Update to starsector bindings, again.

--- src/lynx_left_keyboard.cpp
+++ src/lynx_left_keyboard.cpp
@@ -61,10 +61,10 @@ static const keyboard_joystick_layout prey_joystick = {
 };
 
 static const int starsector_keypad[ROW_NB][COLUMN_NB] = {
-  {NO_KEY, 'f', '5', '1', ' ', KEY_TAB, NO_KEY},
-  {NO_KEY, 'v', '6', '2', NO_KEY, 'c', NO_KEY},
-  {NO_KEY, 'r', '7', '3', NO_KEY, NO_KEY, NO_KEY},
-  {NO_KEY, 't', '8', '4', KEY_LEFT_SHIFT, KEY_LEFT_CTRL, NO_KEY}
+  {NO_KEY, 'r', '5', '1', ' ', KEY_ESC, NO_KEY},
+  {NO_KEY, 'f', '6', '2', NO_KEY, 'c', NO_KEY},
+  {NO_KEY, 't', '7', '3', NO_KEY, NO_KEY, NO_KEY},
+  {NO_KEY, 'v', '8', '4', KEY_LEFT_SHIFT, KEY_LEFT_CTRL, NO_KEY}
 };
 
 static const keyboard_joystick_layout starsector_joystick = {
